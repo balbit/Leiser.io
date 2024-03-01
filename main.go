ef29767a11f29b8bc40df332a39b13b85d2af197
Elliot Liu <elliotliu123@gmail.com>
2024-03-01 17:58:09 -0500

    testing

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+var loadCounter int = 0
+
 func getPort() string {
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -20,11 +22,20 @@ func getPort() string {
 func main() {
 	app := fiber.New()
 
+	app.Use(func(c *fiber.Ctx) error {
+		// Increment the load counter
+		loadCounter++
+		// Pass control to the next middleware/handler
+		return c.Next()
+	})
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
 			"message": "Hello, Railway!",
+			"loads":   loadCounter,
 		})
 	})
+
 
 	app.Listen(getPort())
 }
